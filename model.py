abef7d5251234ea9b6625045c36fb86a0f4190c5
Linna La <linna.la@gmail.com>
2014-07-08 01:42:47 -0700

    added styling for structure, created initial database

---
+++ model.py
@@ -0,0 +1,41 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime
+import datetime
+from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
+
+engine = create_engine("sqlite:///translations.db", echo = False)
+session = scoped_session(sessionmaker(bind=engine,
+                                        autocommit = False,
+                                        autoflush = False))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+# Class definitions
+
+class Project(Base):
+	__tablename__ = "projects"
+
+	#id, title, description, source text
+	id 			= Column(Integer, primary_key = True)
+	title 		= Column(String(50), nullable = False)
+	description = Column(String(200), nullable = False)
+	source_text = Column(String(3000), nullable = False)
+
+class Commit(Base):
+	__tablename__ = "commits"
+
+	#id, project_id, new version, time
+	id = 			Column(Integer, primary_key = True)
+	project_id = 	Column(Integer, ForeignKey('projects.id'), nullable = False)
+	content = 		Column(String(3000), nullable = False)
+	timestamp = 	Column(DateTime, nullable = False)
+
+	project = relationship("Project", backref=backref("Commit"))
+
+	def main():
+		pass
+
+	if __name__ == "__main___":
+		main()
