573db21794f1fa00c59f92387f9ef61f46a03b2f
Linna La <linna.la@gmail.com>
2014-07-10 11:05:00 -0700

    removed commented JS

--- static/js/translate.js
+++ static/js/translate.js
@@ -10,17 +10,3 @@ $(document).ready(function(){
     });
 
 });
-
-// $(document).ready(function(){
-
-//  $("#translation [type='submit']").click(function(e){
-//      e.preventDefault();
-//      var data = $("form").serializedArray();
-//      $.ajax({
-//          type: "POST",
-//          url: "/translate",
-//          data: data
-//      });
-//  });
-
-// });
