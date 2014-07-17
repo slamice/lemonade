853ae7519eb465fd278b133e3777dc16a053d083
Linna La <linna.la@gmail.com>
2014-07-16 18:36:59 -0700

    switching to context_diff

--- text_generator.py
+++ text_generator.py
@@ -1,28 +1,29 @@
 import difflib
+import re
 
-version1 = ["first", "second", "third"]
-version2 = ["first", "replaced second", "third"]
+string1 = "Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. This part is different."
+string2 = "Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Here is a ton of stuff that is the same. Different part is here. Aloha."
 
-d = difflib.Differ()
-# creates a diff object
-diff = d.compare(version1, version2)
-# adds enter between each line of diff info
-connect_diff = '\n'.join(diff)
-print connect_diff
-# makes array of strings of each line
-split_diff = connect_diff.split('\n')
-print split_diff
+array1 = string1.split('. ')
+array2 = string2.split('. ')
 
-# to generate second text from diffs
-additions = []
-deletions = []
+context_diff = difflib.context_diff(array1, array2)
+# context_diff would be stored in the db
 
-for item in split_diff:
-	diff_sign = item[0]
-	if diff_sign == '-':
-		deletions.append(item[2:])
-	elif diff_sign == '+':
-		additions.append(item[2:])
+# context_diff_array = []
 
-print deletions
-print additions
+for line in context_diff:
+	# context_diff_array.append(line)
+	# print line
+	search = re.match("\*{3} .* \*{4}", line)
+	print search
+	# context _diff_array[3] has the line number of changes
+
+
+
+
+
+
+# for each difference:
+# 	array1[line in which it is different] = the new text
+# print '.'.join(array1)
