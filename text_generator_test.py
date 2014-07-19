2c0adc041180eeee89039788dadeee3afd9e66c4
Linna La <linna.la@gmail.com>
2014-07-18 17:22:59 -0700

    creates dictionary of original lines plus relevant diffs

--- text_generator_test.py
+++ text_generator_test.py
@@ -12,18 +12,23 @@ def diff(text1, text2):
     diff_hunks_array = diff_str.split('\n@@ ')
     diff_hunks_array = diff_hunks_array[1:]
 
+    diffs_dict = {}
     for hunk in diff_hunks_array:
         hunk_pieces = hunk.split('\n')
         begin, end = hunk_pieces[0].split(' ')[0][1:].split(',')
         line_pos = (int(begin), int(end))
         # pulls line numbers
+        differences = hunk_pieces[3:]
+
+        diffs_dict[line_pos] = []
 
         diffs = []
-        for line in hunk_pieces[3:]:
+        for line in differences:
             while line[0] == '-' or line[0] == '+':
                 diffs.append(line)
                 break
-        return diffs
+        diffs_dict[line_pos].append(diffs)
+    print diffs_dict
 
 def apply_diffs(text1, diffs):
     new_text = []
@@ -31,6 +36,8 @@ def apply_diffs(text1, diffs):
     i = 0
     while i < (len(text1)-1):
         new_text.append(text1[i])
+        # something is going to change here where
+        # it has to see whether there are diffs at that particular line
         while diffs:
             current = diffs.pop(0)
             if current[0] == '-':
@@ -40,7 +47,7 @@ def apply_diffs(text1, diffs):
     return ' '.join(new_text)
 
 if __name__ == "__main__":
-    text1 = "1 2 3 4 5 6 7"
-    text2 = "1 3' 4' 6', 7'"
+    text1 = "1 2 3 4 5 6 7 a a a a a a a a a zebra"
+    text2 = "1 3' 4' 6', 7' a a a a a a a a a zoo"
     diffs = diff(text1, text2)
-    print apply_diffs(text1, diffs)
+    # apply_diffs(text1, diffs)
