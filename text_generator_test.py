e54af930be3096e10812ae0abf5296de48e99404
Linna La <linna.la@gmail.com>
2014-07-21 17:01:10 -0700

    generates text from original text and diffs stored in tuples, except for last bit

--- text_generator_test.py
+++ text_generator_test.py
@@ -12,7 +12,7 @@ def diff(text1, text2):
     diff_hunks_array = diff_str.split('\n@@ ')
     diff_hunks_array = diff_hunks_array[1:]
 
-    diffs_list = []
+    diffs = []
     for hunk in diff_hunks_array:
         hunk_pieces = hunk.split('\n')
         begin, end = hunk_pieces[0].split(' ')[0][1:].split(',')
@@ -20,7 +20,6 @@ def diff(text1, text2):
         # pulls line numbers
         differences = hunk_pieces[2:]
 
-        diffs = []
         line_num = int(begin) - 1
         for line in differences:
             if line[0] == ' ':
@@ -33,28 +32,28 @@ def diff(text1, text2):
                 elif line[0] == '+':
                     diffs.append((line_num, line[0], line[1:]))
                     break
-        diffs_list.append(diffs)
-    return diffs_list
+    diffs = sorted(diffs)
+    return diffs
+    # this returns a list of tuples: (line number, command, args)
 
 def apply_diffs(text1, diffs):
     new_text = []
     text1 = text1.split()
+
     i = 0
-    # i is equal to line number - 1
     while i < (len(text1)-1):
-        new_text.append(text1[i])
-        # something is going to change here where
-        # it has to see whether there are diffs at that particular line
-        while diffs:
+        while (i + 1) == diffs[0][0]:
             current = diffs.pop(0)
-            if current[0] == '-':
+            if current[1] == '-':
                 i += 1
-            elif current[0] == '+':
-                new_text.append(current[1:])
+            elif current[1] == '+':
+                new_text.append(current[2])
+        new_text.append(text1[i])
+        i += 1
     return ' '.join(new_text)
 
 if __name__ == "__main__":
-    text1 = "1 2 3 4 5 6 7 a a a a a a a a a zebra"
-    text2 = "1 3' 4' 6', 7' a a a a a a a a a zoo"
+    text1 = "1 2 3 4 5 6 7 a a a a a a a a a zebra a a a a a a a zero"
+    text2 = "1 3' 4' 6' 7' a a a a a a a a a zoo a a a a a a a zonk"
     diffs = diff(text1, text2)
-    # apply_diffs(text1, diffs)
+    print apply_diffs(text1, diffs)
