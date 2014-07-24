5f5101f0e55aae8000aeb1b43d0383e7ae98d6f9
Linna La <linna.la@gmail.com>
2014-07-24 11:41:10 -0700

    improved parsing and application of diffs if line number is 0

--- text_generator_test.py
+++ text_generator_test.py
@@ -1,68 +1,103 @@
 import difflib
+import operator
 import nltk.data
+import json
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 def tokenize(text):
     return tokenizer.tokenize(text)
 
-def diff(text1, text2):
+def generate_diffs_json(text1, text2):
+    # creates diff generator
     diff = difflib.unified_diff(text1, text2)
 
+    # conversion to array string
     diff_array = []
     for line in diff:
         diff_array.append(line)
     diff_str = '\n'.join(diff_array)
+
+    # splitting into array of diff hunk
+    # diff_hunks_array[0] is garbage formatting from unified_diff
+    # each item after is a set of diffs for a hunk of diffs
     diff_hunks_array = diff_str.split('\n@@ ')
     diff_hunks_array = diff_hunks_array[1:]
 
+    # for each hunk, pull line numbers for chunks and append relevant diff lines to a list
     diffs = []
+
     for hunk in diff_hunks_array:
         hunk_pieces = hunk.split('\n')
-        if len(hunk_pieces[0]) > 8:
-            begin, end = hunk_pieces[0].split(' ')[0][1:].split(',')
-            differences = hunk_pieces[2:]
-        elif len(hunk_pieces[0]) <= 8:
-            begin = hunk_pieces[0][1]
-            end = hunk_pieces[0][4]
-            differences = hunk_pieces[2:]
+        # hunk_pieces[0] has line numbers
+        # hunk_pieces[1] has an empty line
+        # hunk_pieces[2:] are lines of diffs
 
+        beginning_lines = hunk_pieces[0].split(' ')[0]
+        # if one line of diff, beginning_lines[1:] is where the diffs begin
+        if ',' not in beginning_lines:
+            begin = beginning_lines[1:]
+        # if more than one line of diff, there is a , so have to split and take first #
+        else:
+            begin = beginning_lines.split(',')[0][1:]
+        differences = hunk_pieces[2:]
+
+        # parsing lines of diffs for commands by looping through each line
+        # and if there is a command, adding a dictionary of that command
+        # and appending that dictionary to a list of commands
         line_num = int(begin) - 1
         for line in differences:
+            diff_dict = {}
             if line[0] == ' ':
                 line_num += 1
             while line[0] == '-' or line[0] == '+':
                 if line[0] == '-':
                     line_num += 1
-                    diffs.append((line_num, '-', None))
+                    diff_dict = {'line': line_num, 'cmd': '-', 'text': None}
+                    diffs.append(diff_dict)
                     break
                 elif line[0] == '+':
-                    diffs.append((line_num, line[0], line[1:]))
+                    diff_dict = {'line': line_num, 'cmd': '+', 'text': line[1:]}
+                    diffs.append(diff_dict)
                     break
-    diffs = sorted(diffs)
+
+    # making sure + comes before - so that it doesn't skip lines before it's supposed to
+    diffs = sorted(diffs, key=operator.itemgetter('line','cmd'))
+    diffs = json.dumps(diffs)
+
+    print diffs
     return diffs
-    # this returns a list of tuples: (line number, command, args)
+    # this returns list of dicts with line nums, +/-, and text if cmd is +
+    # eventually this will be put into the database
 
 def apply_diffs(text1, diffs):
+    diffs = json.loads(diffs)
     new_text = []
 
     i = 0
+    # while there are still lines in the original text to loop through
     while i < (len(text1)):
-        while len(diffs) > 0 and (i + 1) == diffs[0][0]:
+        while len(diffs) > 0 and ((i + 1) == diffs[0].get("line") or diffs[0].get("line") == 0):
             current = diffs.pop(0)
-            if current[1] == '-':
+            if current.get("cmd") == '-':
                 i += 1
-            elif current[1] == '+':
-                new_text.append(current[2])
+            elif current.get("cmd") == '+':
+                new_text.append(current.get("text"))
         if 0 <= i and i < len(text1):
             new_text.append(text1[i])
             i += 1
-    return ' '.join(new_text)
+    print ' '.join(new_text)
 
-if __name__ == "__main__":
-    text1 = "Here's a sentence. b. c. d. e. f."
-    text2 = "Here's a sentence. b. c. d. zip. f. zop. zoop."
+def main():
+    # ""
+    # "Alphabet soup. Banana. Hello. No."
+    # "Banana. California. Arkansas. No."
+    text1 = ""
+    text2 = "Hi. Text. Alphabet soup. Banana. Hello. No."
     text1 = tokenize(text1)
     text2 = tokenize(text2)
-    diffs = diff(text1, text2)
-    print apply_diffs(text1, diffs)
+    diffs = generate_diffs_json(text1, text2)
+    apply_diffs(text1, diffs)
+
+if __name__ == "__main__":
+    main()
