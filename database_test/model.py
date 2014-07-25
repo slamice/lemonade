38fcd7fd42d0f0db5f22d457b22391e75a5a8cac
Linna La <linna.la@gmail.com>
2014-07-25 11:53:36 -0700

    renamed database

--- database_test/model.py
+++ database_test/model.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 
-engine = create_engine("sqlite:///translations.db", echo = False)
+engine = create_engine("sqlite:///database.db", echo = False)
 session = scoped_session(sessionmaker(bind=engine,
                                         autocommit = False,
                                         autoflush = False))
