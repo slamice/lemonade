38dcb6afd5501d2276d26aa0967223a751b26762
Linna La <linna.la@gmail.com>
2014-07-24 17:10:53 -0700

    remade database, added parent_id and diffs

--- model.py
+++ model.py
@@ -1,10 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Integer, String, DateTime
-import datetime
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 
-engine = create_engine("sqlite:///translations.db", echo = False)
+engine = create_engine("sqlite:///database.db", echo = False)
 session = scoped_session(sessionmaker(bind=engine,
                                         autocommit = False,
                                         autoflush = False))
@@ -15,28 +14,29 @@ Base.query = session.query_property()
 # Class definitions
 
 class Project(Base):
-	__tablename__ = "projects"
+    __tablename__ = "projects"
 
-	#id, title, description, source text
-	id 			= Column(Integer, primary_key = True)
-	title 		= Column(String(50), nullable = False)
-	description = Column(String(200), nullable = False)
-	source_text = Column(String(3000), nullable = False)
+    #id, title, description, source text
+    id          = Column(Integer, primary_key = True)
+    title       = Column(String(50), nullable = False)
+    description = Column(String(200), nullable = False)
+    source_text = Column(Text, nullable = False)
 
 class Commit(Base):
-	__tablename__ = "commits"
+    __tablename__ = "commits"
 
-	#id, project_id, new version, time
-	id 			= Column(Integer, primary_key = True)
-	project_id 	= Column(Integer, ForeignKey('projects.id'), nullable = False)
-	timestamp 	= Column(DateTime, nullable = False)
-	translation = Column(String(3000), nullable = False)
-	message 	= Column(String(140), nullable = False)
+    #id, project_id, parent_id, time
+    id          = Column(Integer, primary_key = True)
+    project_id  = Column(Integer, ForeignKey('projects.id'), nullable = False)
+    parent_id   = Column(Integer, nullable = True)
+    timestamp   = Column(DateTime, nullable = False)
+    message     = Column(String(140), nullable = False)
+    diffs       = Column(Text, nullable = False)
 
-	project = relationship("Project", backref=backref("Commit"))
+    project = relationship("Project", backref=backref("Commit"))
 
 def main():
-	pass
+    pass
 
 if __name__ == "__main___":
-	main()
+    main()
