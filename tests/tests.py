4a95553351fbf4cc74d0e010a4b4a5b77d673702
Linna La <linna.la@gmail.com>
2014-07-30 16:40:03 -0700

    imports functions instead of copy/pasta

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,10 @@
-# FOR GENERATE_DIFFS
-import difflib
-import operator
 import json
-import pdb
 
-# FOR APPLY_DIFFS
-import nltk
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+# IMPORTING FUNCTIONS
+import sys
+sys.path.append("../")
+from generators import generate_diffs
+from generators import apply_diffs
 
 # ------------------------------------------#
 #                                           #
@@ -14,109 +12,6 @@ tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 #  and outputs a json list of dictionaries  #
 #                                           #
 # ------------------------------------------#
-
-def generate_diffs(before_tokens, after_tokens):
-    print "DIFFING TOKENS..."
-
-    # creates diff generator
-    diff = difflib.unified_diff(before_tokens, after_tokens)
-
-    # conversion to array to string
-    diff_array = []
-    for line in diff:
-        diff_array.append(line)
-    diff_str = '\n'.join(diff_array)
-
-    # splitting into array of diff hunk
-    # diff_hunks_array[0] is garbage formatting from unified_diff
-    # each item after is a set of diffs for a hunk of diffs
-    diff_hunks_array = diff_str.split('\n@@ ')
-    diff_hunks_array = diff_hunks_array[1:]
-
-    # # for each hunk, pull line numbers for hunks and append relevant diff lines to a list
-    diffs = []
-
-    for hunk in diff_hunks_array:
-        hunk_pieces = hunk.split('\n')
-        # hunk_pieces[0] has line numbers
-        # hunk_pieces[1] has an empty line
-        # hunk_pieces[2:] are lines of diffs
-
-        before_lines = hunk_pieces[0].split(' ')[0]
-        # -begin_line,end_line
-
-        # if multi-line diffs
-        if ',' in before_lines:
-            before_start = before_lines.split(',')[0][1:]
-            before_end = before_lines.split(',')[1][0:]
-        # if single-line diffs
-        else:
-            before_start = before_lines[1:]
-            before_end = before_lines[1:]
-
-        after_lines = hunk_pieces[0].split(' ')[1]
-        # +begin_line,end_line
-        if ',' in after_lines:
-            # print after_lines
-            after_start = after_lines.split(',')[0][1:]
-            after_end = after_lines.split(',')[1][0:]
-        # if single-line diffs
-        else:
-            after_start = after_lines[1:]
-            after_end = after_lines[1:]
-
-        before_start = int(before_start)
-        before_end = int(before_end)
-        after_start = int(after_start)
-        after_end = int(after_end)
-
-        differences = hunk_pieces[2:]
-        # this is a set of differences for one hunk
-        # ex: [' A', ' B', ' C', '-G', '-H', '-I', '+D', '+E', '+F', ' X', '-Y', ' Z']
-
-        # parsing lines of diffs for commands by looping through each line
-        # if there is a command, adding a dictionary of that command
-        # and appending that dictionary to a list of commands
-
-        i = before_start - 1
-        j = after_start - 1
-
-        for line in differences:
-            print line
-            diff_dict = {}
-
-            # increment both i and j
-            if line[0] == ' ':
-                i += 1
-                j += 1
-
-            # increment i
-            elif line[0] == '-':
-                print "Deletion detected."
-                # increments beforehand because line number is actually (i + 1)
-                i += 1
-
-                # text token being deleted
-                if line[1:]:
-                    diff_dict = {'after_line': j, 'before_line': i, 'cmd': '-', 'text': None}
-                    diffs.append(diff_dict)
-                    
-                # empty token being deleted
-                else:
-                    print "Say no to whitespace."
-
-            # increment j
-            elif line[0] == '+':
-                print "Addition detected."
-                # increments beforehand because line number is actually (j + 1)
-                j += 1
-
-                diff_dict = {'after_line': j, 'before_line': i, 'cmd': '+', 'text': line[1:]}
-                diffs.append(diff_dict)
-
-    # conversion of diffs into json object (list of dictionaries) to be stored in database
-    diffs = json.dumps(diffs)
-    return diffs
 
 def test_diff_gen_seed():
     print "\n\nSeeding empty token, then adding tokens."
@@ -244,63 +139,9 @@ def test_diff_gen_add_first():
 #                                           #
 # ------------------------------------------#
 
-# ** WILL take a list of tokens, and output tokens,
+# ** takes a list of tokens, and output tokens,
 # ** and when text is needed as a string, i.e. displayed on editor,
 # ** it can be joined into a string in the controller.
-
-def apply_diffs(tokens, diffs):
-    print "APPLYING DIFFS..."
-    new_tokens = []
-    diffs = json.loads(diffs)
-
-    i = 0
-    j = 0
-    diff_index = 0
-
-    # while there are diffs remaining
-    while diff_index < len(diffs):
-        current = diffs[diff_index]
-
-        # 1. cmd is +, j matches and increase j, diff_index
-        if current.get("cmd") == "+" and current.get("after_line") == j + 1:
-            add_token = current.get("text")
-            new_tokens.append(add_token)
-
-            print "\nAdded existing token."
-            print add_token
-            diff_index += 1
-            j += 1
-
-        # 2. cmd is -, i matches and increase i, diff_index
-        elif current.get("cmd") == "-" and current.get("before_line") == i + 1:
-
-            print "\nSkipped deleted line."
-            diff_index += 1
-            i += 1
-        # 3. diffs exist, but none match so copy and increase both
-        else:
-            existing_token = tokens[i]
-            new_tokens.append(existing_token)
-
-            print "\nCopied existing token."
-            print existing_token
-            i += 1
-            j += 1
-
-    # copy remaining from tokens to output
-    while i < len(tokens):
-        remaining_token = tokens[i]
-        if remaining_token == "":
-            print "Ignoring empty token."
-        else:
-            new_tokens.append(remaining_token)
-            print "\nCopied remaining tokens."
-            print remaining_token
-        i += 1
-
-    print "\nReturning new tokens:"
-    print new_tokens
-    return new_tokens
 
 def test_apply_seed():
     print "\n\nTake an empty string and add text."
@@ -415,32 +256,32 @@ def test_apply_add_first():
         print "Cool, test passed. Good job, you!"
 
 def main():
-    # test_diff_gen_seed()
-    # test_diff_gen_subtract()
-    # test_diff_gen_addition()
-    # test_diff_gen_switch_token()
-    # test_diff_gen_alternate()
-    # test_diff_gen_delete_first()
-    # test_diff_gen_add_first()
+    test_diff_gen_seed()
+    test_diff_gen_subtract()
+    test_diff_gen_addition()
+    test_diff_gen_switch_token()
+    test_diff_gen_alternate()
+    test_diff_gen_delete_first()
+    test_diff_gen_add_first()
 
-    # test_apply_seed()
-    # test_apply_subtract()
-    # test_apply_addition()
-    # test_apply_switch_token()
-    # test_apply_alternate()
-    # test_apply_delete_first()
-    # test_apply_add_first()
+    test_apply_seed()
+    test_apply_subtract()
+    test_apply_addition()
+    test_apply_switch_token()
+    test_apply_alternate()
+    test_apply_delete_first()
+    test_apply_add_first()
 
-    # EXAMPLE: GENERATE DIFFS
-    before_tokens = ["A", "B", "C", "G", "H", "I", "X", "Y", "Z"]
-    after_tokens = ["A", "B", "C", "D", "E", "F", "X", "Z"]
-    generate_diffs(before_tokens, after_tokens)
+    # # EXAMPLE: GENERATE DIFFS
+    # before_tokens = ["A", "B", "C", "G", "H", "I", "X", "Y", "Z"]
+    # after_tokens = ["A", "B", "C", "D", "E", "F", "X", "Z"]
+    # generate_diffs(before_tokens, after_tokens)
 
-    # EXAMPLE: APPLY DIFFS
-    tokens = ["A", "B", "C", "G", "H", "I", "X", "Y", "Z"]
-    diffs = [{"before_line": 4, "cmd": "-", "after_line": 3, "text": None}, {"before_line": 5, "cmd": "-", "after_line": 3, "text": None}, {"before_line": 6, "cmd": "-", "after_line": 3, "text": None}, {"before_line": 6, "cmd": "+", "after_line": 4, "text": "D"}, {"before_line": 6, "cmd": "+", "after_line": 5, "text": "E"}, {"before_line": 6, "cmd": "+", "after_line": 6, "text": "F"}, {"before_line": 8, "cmd": "-", "after_line": 7, "text": None}]
-    diffs = json.dumps(diffs)
-    apply_diffs(tokens, diffs)
+    # # EXAMPLE: APPLY DIFFS
+    # tokens = ["A", "B", "C", "G", "H", "I", "X", "Y", "Z"]
+    # diffs = [{"before_line": 4, "cmd": "-", "after_line": 3, "text": None}, {"before_line": 5, "cmd": "-", "after_line": 3, "text": None}, {"before_line": 6, "cmd": "-", "after_line": 3, "text": None}, {"before_line": 6, "cmd": "+", "after_line": 4, "text": "D"}, {"before_line": 6, "cmd": "+", "after_line": 5, "text": "E"}, {"before_line": 6, "cmd": "+", "after_line": 6, "text": "F"}, {"before_line": 8, "cmd": "-", "after_line": 7, "text": None}]
+    # diffs = json.dumps(diffs)
+    # apply_diffs(tokens, diffs)
 
 if __name__ == "__main__":
     main()
