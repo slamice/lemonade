c613907f8bab508987c45f6dfd64d73cd8f0129a
Linna La <linna.la@gmail.com>
2014-08-01 16:50:54 -0700

    styling for editor

--- static/js/translate.js
+++ static/js/translate.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 
+    // toggle sidebar
+    $("#nav_open").click(function(){
+        $("#sidebar").toggle('slide', 500);
+    });
+
+    $("#nav_close").click(function(){
+        $("#sidebar").toggle('slide', 500);
+    });
+
+    // focus on textarea + move cursor to end of text
+    var el = $("#translation textarea").get(0);
+    var elemLen = el.value.length;
+
+    el.selectionStart = elemLen;
+    el.selectionEnd = elemLen;
+    el.focus();
+
+    // magicat!
+    $("#cat-button").click(function(e){
+        e.preventDefault();
+        var width = $(window).width();
+        var cat_width = $("#cat").width();
+        $("#cat").animate({left: width-cat_width}, 1000);
+    });
+
+    // save commit + prevent refresh
     $("#translation").submit(function(e){
         $.ajax({
             type: "POST",
